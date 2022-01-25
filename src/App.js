f05f7167d5da38463e27ce4a6419be4a3a1fded3
AdilMahmoudRion <adilmahmoudrion@gmail.com>
2022-01-25 21:06:19 +0600

    navigation added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import Home from "./Components/Pages/Home/Home";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <>
+      <Home></Home>
+    </>
   );
 }
 
